f3eaf2e99909718074459721deedcff4b5a65565
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-04 23:37:18 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -19,3 +19,20 @@ SCENARIO("SP: get", "[get]") {
 
 	REQUIRE(ptr == sp.get());
 }
+SCENARIO("SP: use_count", "[use_count]") {
+	size_t * ptr = new size_t[10];
+	shared_ptr<size_t> f_sp(ptr);
+
+	REQUIRE(f_sp.count() == 1);
+
+	shared_ptr<size_t> s_sp(f_sp);
+
+	REQUIRE(f_sp.count() == 2);
+	REQUIRE(s_sp.count() == 2);
+
+	shared_ptr<size_t> t_sp(s_sp);
+
+	REQUIRE(f_sp.count() == 3);
+	REQUIRE(s_sp.count() == 3);
+	REQUIRE(t_sp.count() == 3);
+}
