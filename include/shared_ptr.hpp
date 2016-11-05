c823817f0173082ded383b80ccb317ea22843142
Balanar300hp <kudashov.vlad@yandex.ru>
2016-11-05 19:25:43 +0300

    Update shared_ptr.hpp

--- include/shared_ptr.hpp
+++ include/shared_ptr.hpp
@@ -66,7 +66,7 @@ template<typename T>
     template<typename T>
     auto shared_ptr<T>::operator =(shared_ptr && other) -> shared_ptr &
     {
-        swap(other);
+        this->swap(other);
 	return *this;
     }
 
